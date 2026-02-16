6bc74f1e80566d5d33de030c885cbf799d88f46b
vip23anchib <barman23vipanchi@gmail.com>
2026-02-16 16:54:53 +0000

    67. adding binary numbers

---
+++ 67.addBinary.cpp
@@ -0,0 +1,32 @@
+// adding binary numbers
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        int carry = 0;
+
+        string result = "";
+        
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            
+            if (i >= 0) {
+                sum += a[i] - '0';  //add & convert character to integer 
+                i--;
+            }
+        
+            if (j >= 0) {
+                sum += b[j] - '0';
+                j--;
+            }
+            
+            result += (sum % 2) + '0';  //gets current binary digit and converts to int
+            carry = sum / 2;      // update carry     
+        }
+        
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
