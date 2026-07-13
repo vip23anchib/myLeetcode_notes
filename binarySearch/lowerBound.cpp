5e092d658d5f19cfd42d506ffc86622212eaaf89
vip23anchib <barman23vipanchi@gmail.com>
2026-07-13 23:10:54 +0530

    Add lowerBound function for binary search
    Implement binary search lower bound function to find the index of the first element greater than or equal to the target.

---
+++ binarySearch/lowerBound.cpp
@@ -0,0 +1,38 @@
+// Returns index of first element >= target.
+// Returns nums.size() if no such element exists.
+// Time: O(log n)
+
+int lowerBound(vector<int>& nums, int target) {
+
+    int low = 0;
+    int high = nums.size() - 1;
+    int ans = nums.size();
+
+    while (low <= high) {
+
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] >= target) {
+            ans = mid;
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+//STL LOWER BOUND:
+// Returns iterator to first element >= target.
+auto it = lower_bound(nums.begin(), nums.end(), target);
+
+//get index 
+int index = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+
+//example:
+vector<int> nums = {1,2,2,2,4,5};
+
+int index = lower_bound(nums.begin(), nums.end(), 2) - nums.begin();
+// index = 1
